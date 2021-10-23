6bb9e1af9a92a5c395ba5ef1598767e0e878d47f
niti123mishra <34702357+niti123mishra@users.noreply.github.com>
2021-10-24 00:29:57 +0530

    Create FormattedOutput.java

---
+++ Introduction/FormattedOutput.java
@@ -0,0 +1,43 @@
+/*
+Input Format
+Every line of input will contain a String followed by an integer. 
+Each String will have a maximum of 1010 alphabetic characters, and each integer will be in the inclusive range from 00 to 999999.
+Output Format
+In each line of output there should be two columns: 
+The first column contains the String and is left justified using exactly 1515 characters. 
+The second column contains the integer, expressed in exactly 33 digits; if the original input has less than three digits, you must pad your output's leading digits with zeroes.
+
+Sample Input
+java 100
+cpp 65
+python 50
+
+Sample Output
+================================
+java           100 
+cpp            065 
+python         050 
+================================
+*/
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String st = sc.next();
+            int n = sc.nextInt();
+            System.out.printf("%-14s %03d\n",st,n);
+        }  
+            System.out.println("================================");
+            
+        
+        
+    }
+}
